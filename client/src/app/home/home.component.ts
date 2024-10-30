bf9917697f28edde1100474aec23898b30fd6319
rodribscura <rodribscura@gmail.com>
2024-10-30 14:13:38 -0300

    End of section 7

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -30,7 +30,7 @@ export class HomeComponent implements OnInit {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
-      complete: () => console.log('Request has completed')
+      complete: () => console.log()
     })
     }
 
