dcf202350363b1638427de51d179092590c41407
rodribscura <rodribscura@gmail.com>
2024-12-14 21:37:19 -0300

    End of Section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
-﻿using API.Data;
+﻿using System.Collections;
+using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,23 +11,22 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 
-
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
   
-    [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers ()  //IEnumerable: Lista de AppUsers
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers ()  //IEnumerable: Lista de AppUsers
     {
-        var users = await context.Users.ToListAsync();
-        return users;
+        var users = await userRepository.GetMembersAsync();
+
+        return Ok(users);
     }
 
-    [Authorize]   
-    [HttpGet("{id:int}")]  // api/users/3
-    public async Task<ActionResult<AppUser>> GetUser(int id)  
+    [HttpGet("{username}")]  // api/users/3
+    public async Task<ActionResult<MemberDto>> GetUser(string username)  
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetMemberAsync(username);
         
         if (user == null) return NotFound();
 
